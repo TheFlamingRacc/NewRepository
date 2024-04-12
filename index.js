e8e2b1e74a9944e2e104758426affcfedcf742a8
TheFlamingRacc <166836060+TheFlamingRacc@users.noreply.github.com>
2024-04-12 17:53:15 +0300

    Console log

--- index.js
+++ index.js
@@ -1,13 +1,1 @@
-const data = [
-    { category: 'fruit', name: 'apple', quantity: 5 },
-    { category: 'vegetable', name: 'carrot', quantity: 8 },
-    { category: 'fruit', name: 'banana', quantity: 3 },
-    { category: 'vegetable', name: 'lettuce', quantity: 2 }
-  ];
-
-  const allVegetables = data.reduce((acc, x) => {
-    if (x.category === vegetable) {
-        return acc.concat(x)
-    }
-    return 
-  }, []);
+console.log("Hello, World!");
