b8ac9f1e6bdfa233ee4d2310ad9606e13d7d717e
TheFlamingRacc <166836060+TheFlamingRacc@users.noreply.github.com>
2024-05-23 20:51:15 +0300

    :)

--- 23.05/index.js
+++ 23.05/index.js
@@ -1,34 +1,34 @@
 function CurrencyConverter(num) {
-    this.exchangeRate = num;
-  
-    this.convert = function (amountInHryvnia, currencyName) {
-      const amountInTargetCurrency = amountInHryvnia / this.exchangeRate;
-      return `${amountInHryvnia} UAH is equal to ${amountInTargetCurrency.toFixed(
-        2
-      )} ${currencyName}`;
-    };
-  
-    this.getInfo = function () {
-      return Exchange Rate ${this.exchangeRate};
-    };
-  
-    this.updateExchangeRate = function (newRate) {
-      this.exchangeRate = newRate;
-    };
-  }
-  
-  const euroConverter = new CurrencyConverter(44);
-  
-  console.log(euroConverter.getInfo());
-  
-  console.log(euroConverter.convert(300, 'euro'));
-  
-  const dollarConverter = new CurrencyConverter(38);
-  
-  console.log(dollarConverter.convert(300, 'dollar'))
-  
-  dollarConverter.updateExchangeRate(5);
-  
-  console.log(dollarConverter.getInfo())
-  
-  console.log(dollarConverter.convert(200, 'dollar'));
+  this.exchangeRate = num;
+
+  this.convert = function (amountInHryvnia) {
+    const amountInTargetCurrency = amountInHryvnia / this.exchangeRate;
+    return `${amountInHryvnia} UAH is equal to ${amountInTargetCurrency.toFixed(
+      2
+    )} of your currency`;
+  };
+
+  this.getInfo = function () {
+    return `Exchange Rate ${this.exchangeRate}`;
+  };
+
+  this.updateExchangeRate = function (newRate) {
+    this.exchangeRate = newRate;
+  };
+}
+
+const euroConverter = new CurrencyConverter(44);
+
+console.log(euroConverter.getInfo());
+
+console.log(euroConverter.convert(300, "euro"));
+
+const dollarConverter = new CurrencyConverter(38);
+
+console.log(dollarConverter.convert(300, "dollar"));
+
+dollarConverter.updateExchangeRate(5);
+
+console.log(dollarConverter.getInfo());
+
+console.log(dollarConverter.convert(200, "dollar"));
